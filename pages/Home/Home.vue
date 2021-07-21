440331146d5ed36e7d57e3d4b1008b9a48aec087
zhangjun <1548293573@qq.com>
2021-07-21 17:56:52 +0800

    first commit

---
+++ pages/Home/Home.vue
@@ -0,0 +1,25 @@
+//主页面
+<template>
+	<view>
+		<text>{{title}}</text>
+	</view>
+</template>
+
+<script lang="ts">
+	import {
+		Component,
+		Vue,
+		Watch
+	} from "vue-property-decorator";
+
+	@Component({
+		name: 'Home'
+	})
+	export default class Home extends Vue {
+		private title = "这就是主页面";
+	}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ pages/Order/Order.vue
@@ -0,0 +1,25 @@
+//订单页
+<template>
+	<view>
+		<text>{{title}}</text>
+	</view>
+</template>
+
+<script lang="ts">
+	import {
+		Component,
+		Vue,
+		Watch
+	} from "vue-property-decorator";
+
+	@Component({
+		name: 'Order'
+	})
+	export default class Order extends Vue {
+		private title = "这就是订单页";
+	}
+</script>
+
+<style lang="less">
+
+</style>
